87709b3f16d3444a32e5c596ad0caa7b620fe90c
KarlitosD <carlosdcampos.04@gmail.com>
2022-07-12 05:53:53 -0300

    add submit action and a modal

--- src/components/FormUser.jsx
+++ src/components/FormUser.jsx
@@ -1,4 +1,5 @@
-import { Form } from "react-bootstrap"
+import { useState } from "react"
+import { Form, Button, Modal } from "react-bootstrap"
 
 export function Input({ text, placeholder, type, name }) {
     return (
@@ -10,17 +11,37 @@ export function Input({ text, placeholder, type, name }) {
 }
 
 export function FormUser() {
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleSubmit = event => {
+        event.preventDefault()
+        setShow(true)
+    }
     return (
-        <Form>
-            <Input name={"name"} text={"Nombre"} placeholder={"Ingrese su nombre"} type="text" />
-            <Input name={"surname"} text={"Apellido"} placeholder={"Ingrese su apellido"} type="text" />
-            <Input name={"email"} text={"Correo Electrónico"} placeholder={"Ingrese su correo electrónico"} type="email" />
-            <Input name={"phone_num"} text={"Número de teléfono"} placeholder={"Ingrese su número de teléfono"} type="number" />
-            <Input name={"dni"} text={"DNI"} placeholder={"Ingrese su DNI"} type="number" />
-            <Input name={"people_amount"} text={"Cantidad de personas a asociar"} placeholder={"Ingrese la cantidad de personas a asociar"} type="number" />
-            {/* <Button variant="primary" type="submit">
-            Submit
-          </Button> */}
-        </Form>
+        <>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Header closeButton>
+                    {/* <Modal.Title>Modal heading</Modal.Title> */}
+                </Modal.Header>
+                <Modal.Body>
+                    <p className="fs-3">Gracias por volverse socio de los Bomberos Voluntarios de Lanus.</p>
+                </Modal.Body>
+
+            </Modal>
+            <Form onSubmit={handleSubmit} className="mt-4">
+                <Input name="name" text="Nombre" placeholder="Ingrese su nombre" type="text" />
+                <Input name="surname" text="Apellido" placeholder="Ingrese su apellido" type="text" />
+                <Input name="email" text="Correo Electrónico" placeholder="Ingrese su correo electrónico" type="email" />
+                <Input name="phone_num" text="Número de teléfono" placeholder="Ingrese su número de teléfono" type="number" />
+                <Input name="dni" text="DNI" placeholder="Ingrese su DNI" type="number" />
+                <Input name="people_amount" text="Cantidad de personas a asociar" placeholder="Ingrese la cantidad de personas a asociar" type="number" />
+                {/* <Button variant="danger" onClick={() => setShow(true)}>
+                    Submit
+                </Button> */}
+                <Button variant="danger" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </>
     )
 }
