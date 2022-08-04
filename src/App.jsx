a45f8ae2941d7e3bca1aab87da632aadde5b34bf
KarlitosD <carlosdcampos.04@gmail.com>
2022-08-04 15:25:29 -0300

    use home component page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { Redirect, Route } from "wouter";
 import { Container, Navbar } from "react-bootstrap";
 import { supabase } from "./supabase";
 import { FormUser } from "./components/FormUser";
-
+import { Home } from "./pages/Home"
+// import { Info } from "./pages/Info"
 async function getUsers() {
   const users = await supabase.from("users").select("name");
   return users;
@@ -23,11 +24,12 @@ function App() {
       </Navbar>
       <Container>
         <Route path="/">
-          <Redirect to="/form" />
+          <Home />
         </Route>
         <Route path="/form">
-          <FormUser />
+
         </Route>
+
       </Container>
     </>
   );
