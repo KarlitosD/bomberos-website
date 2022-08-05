a6c8afa99feeeb2d63ae09e3df2fb7fc9d6f8c55
KarlitosD <carlosdcampos.04@gmail.com>
2022-08-05 18:02:41 -0300

    limpieza del archivo app

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 // import { useEffect } from "react";
-import { Redirect, Route } from "wouter";
-import { Container, Navbar } from "react-bootstrap";
-import { supabase } from "./supabase";
-import { FormUser } from "./components/FormUser";
+// import { supabase } from "./supabase";
+import { Route } from "wouter";
+import { Form } from "./pages/form"
 import Home from "./pages/Home"
-// import { Info } from "./pages/Info"
-async function getUsers() {
-  const users = await supabase.from("users").select("name");
-  return users;
-}
+import Info from "./pages/info"
+
 
 function App() {
-  // useEffect(() => {
-  //   getUsers().then((res) => console.log(res.data));
-  // }, []);
 
   return (
     <>
