98c3d171e01a32d443aacfefb1efd37e7d1a35bc
KarlitosD <carlosdcampos.04@gmail.com>
2022-08-06 19:44:40 -0300

    correccion de getImgUrl al momento de hacer el "build"

--- src/utils/getImgUrl.js
+++ src/utils/getImgUrl.js
@@ -1,8 +1,3 @@
 export function getImgUrl(file) {//funcion para nico
-    const url = new URL(import.meta.url)
-    return new URL(`/src/assets/img/${file}`, url).href
+    return new URL(`/src/assets/img/${file}`, import.meta.url).href
 }
-// export function getImgUrlFacha(file) {//funcion para nizzo y ciro
-//     // console.log({ file, path: import.meta.url })
-//     return new URL(`../../assets/img/${file}`, import.meta.url).href
-// }
