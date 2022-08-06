0d65a3019f720e05c91a00e6091ed1ac3214b5b6
Juan Dou <joaconizzo@gmail.com>
2022-08-06 03:21:49 -0300

    agregado header a form y funcion imagenes facha

--- src/utils/getImgUrl.js
+++ src/utils/getImgUrl.js
@@ -1,4 +1,8 @@
-export function getImgUrl(file) {
+export function getImgUrl(file) {//funcion para nico
     // console.log({ file, path: import.meta.url })
     return new URL(`../assets/img/${file}`, import.meta.url).href
+}
+export function getImgUrlFacha(file) {//funcion para nizzo y ciro
+    // console.log({ file, path: import.meta.url })
+    return new URL(`.../assets/img/${file}`, import.meta.url).href
 }
