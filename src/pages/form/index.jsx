a0fbadec0d2b19d6832e3aa5af7ec22f778a6012
Juan Dou <joaconizzo@gmail.com>
2022-08-06 22:05:03 -0300

    añadido css al form

--- src/pages/form/index.jsx
+++ src/pages/form/index.jsx
@@ -5,36 +5,40 @@ export function Form() {
   return (
     <>
       <Header />
-      <form>
-        <label>
-          Nombre/s:
-          <input name="name" placeholder="Ingrese su nombre" type="text" />
-        </label>
-        <label>
-          Apellido/s:
-          <input name="surname" placeholder="Ingrese su apellido" type="text" />
-        </label>
-        <label>
-          Correo Electrónico:
-          <input
-            name="email"
-            placeholder="Ingrese su correo electrónico"
-            type="email"
-          />
-        </label>
-        <label>
-          Número de Teléfono:
-          <input
-            name="phone_num"
-            placeholder="Ingrese su número de teléfono"
-            type="tel"
-          />
-        </label>
-        <label>
-          DNI:
-          <input name="dni" placeholder="Ingrese su DNI" type="number" />
-        </label>
-        <button>Enviar</button>
+      <form id="mainForm">
+        <div id="box">
+          <div className="row">
+            <label className="rowLabel">
+              Nombre:
+              <input className="rowInput" name="name" type="text" />
+            </label>
+          </div>
+          <div className="row">
+            <label className="rowLabel">
+              Apellido:
+              <input className="rowInput" name="surname" type="text" />
+            </label>
+          </div>
+          <div className="row">
+            <label className="rowLabel">
+              Correo Electrónico:
+              <input className="rowInput" name="email" type="email" />
+            </label>
+          </div>
+          <div className="row">
+            <label className="rowLabel">
+              Número de Teléfono:
+              <input className="rowInput" name="phone_num" type="tel" />
+            </label>
+          </div>
+          <div className="row">
+            <label className="rowLabel">
+              DNI:
+              <input className="rowInput" name="dni" type="number" />
+            </label>
+          </div>
+          <button id="btnSend">Enviar</button>
+        </div>
       </form>
     </>
   );
