1548ebc4299409bf91b65b531079744c0eb34b97
ParadigmaRama <ramirogimenezcontacto@gmail.com>
2022-08-12 09:35:13 -0300

    Hecho el header y agregue imagen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,45 @@ export function Header() {
     <>
       <div className="blankSpace">
       </div>
-      <header>
-        <div>
-          <img className="logo" src={getImgUrl("logo.png")}/>
-        </div>
-      </header>
+         <header>
+            <span className="Nav">
+              <h2>
+                  <a href="/">Inicio</a>
+                </h2>
+            </span>
+            <span className="Nav">
+                <h2>
+                  <a href="/form">Formulario</a>
+                </h2>
+            </span>
+            <div>
+              <a href="/">
+                <img className="logo" src={getImgUrl("logo.png")}/>
+              </a>
+            </div>
+            <span className="Nav"> 
+              <h2>
+                  <a href="/mp">Donaciones</a>
+                </h2>
+            </span>
+            <span className="Nav"> 
+              <h2>
+                <a href="/info">Quienes somos</a>
+              </h2>
+            </span>
+            <div className="Menu">
+                <ul>
+                  <li>
+                    <img className="icono" src={getImgUrl("Menu.png")}></img>
+                    <ul>
+                      <li>Perfil</li>
+                      <li>Configuracion</li>
+                      <li></li>
+                    </ul>
+                  </li>
+                </ul>
+            </div>
+          </header>
     </>
   );
 }
