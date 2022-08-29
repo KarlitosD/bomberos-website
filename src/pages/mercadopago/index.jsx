85dfc8f8c0baaf017dc0328baf0ee8322ebb2732
KarlitosD <carlosdcampos.04@gmail.com>
2022-08-29 05:14:26 -0300

    remplazar anon key fija por variable de entorno

--- src/pages/mercadopago/index.jsx
+++ src/pages/mercadopago/index.jsx
@@ -17,7 +17,7 @@ export function MercadoPago() {
         const options = {
             method: 'POST',
             headers: {
-              "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs',
+              "Authorization": `Bearer ${import.meta.env.VITE_SUPABASE_ANON_KEY}`,
             },
             body: JSON.stringify(example)
           };
