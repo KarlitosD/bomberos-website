1af83b4ccc8e1755d39077eaf3e4974d5fb5cb04
KarlitosD <carlosdcampos.04@gmail.com>
2022-08-29 04:21:21 -0300

    probando endpoint para la subscripcion de mercadopago

--- src/pages/mercadopago/index.jsx
+++ src/pages/mercadopago/index.jsx
@@ -1,32 +1,38 @@
-import { mp } from "@/mercadopago"
-import { useLayoutEffect, useRef } from "react";
+import { } from "react";
+import { supabase } from "../../supabase.js"
 
-const bricksBuilder = mp.bricks();
+const example = {
+    "email": "test_user_36861694@testuser.com",
+    "amount": 100
+}
 
 export function MercadoPago() {
-useLayoutEffect(() => {
-    console.log("Hola")
-    bricksBuilder.create("cardPayment", "cardPaymentBrick_container", {
-        initialization: {
-            amount: 10,
-        },
-        callbacks: {
-            onsubmit: async formData => {
-                console.log(formData)
-                return
+
+    const handleClick = async () => {
+        //TODO: Deploy supabase function
+        // const { data, error } = await supabase.functions.invoke("mercadopago", {
+        //     body: JSON.stringify(example)
+        // })
+        // console.log(data)
+        const options = {
+            method: 'POST',
+            headers: {
+              "Authorization": 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24ifQ.625_WdcF3KHqz5amU0x2X5WWHP-OEs_4qj0ssLNHzTs',
             },
-            onReady() {
-                console.log("ready")
-            },
-            onError(error){
-                console.error(error.message)
-            }
-        }
-    }).then(() => console.log("hola"))
-  }, [])
-  return (
-    <div id="cardPaymentBrick_container"></div>
-  )
+            body: JSON.stringify(example)
+          };
+          
+        const { init_point, pago } = await fetch('http://localhost:54321/functions/v1/', options)
+            .then(response => response.json())
+            .catch(err => console.error(err));
+        
+        window.open(init_point)
+    }
+    return (
+        <>
+            <button onClick={handleClick}>Click</button>
+        </>
+    )
 }
 
 
