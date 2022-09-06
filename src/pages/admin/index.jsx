0ec4bf3e7fb4103389ef0ec8f1bd2e6a8064f6bc
Juan Dou <joaconizzo@gmail.com>
2022-09-06 04:13:12 -0300

    agregado lector base de datos en admin

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -13,13 +13,43 @@ export function Admin() {
     const data = await getAssociates();
     setAssociates(data);
   }
-  useEffect(() =>{
-    console.log("data:", associates)
-  },[associates])
-  return(
+  useEffect(() => {
+    console.log("data:", associates);
+  }, [associates]);
+  return (
     <>
       <Header />
-      
+      <div className="blank"></div>
+      <table>
+        <thead>
+          <tr>
+            <th>dni</th>
+            <th>nombre</th>
+            <th>apellido</th>
+            <th>correo electrónico</th>
+            <th>dirección</th>
+            <th>número de teléfono</th>
+            <th>número de socio</th>
+            <th>aprobado</th>
+          </tr>
+        </thead>
+        <tbody>
+          {associates.map((associate) => (
+            <tr key={associate.dni}>
+              <td>{associate.dni}</td>
+              <td>{associate.name}</td>
+              <td>{associate.surname}</td>
+              <td>{associate.email}</td>
+              <td>{associate.address}</td>
+              <td>{associate.phone_num}</td>
+              <td>{associate.associate_num}</td>
+              <td>
+                <input type="checkbox" checked={associate.approved} disabled />
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 }
