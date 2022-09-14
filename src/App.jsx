9777e6eb2a5a0f133c670be060a20bcf20ade689
Ciro <insaurraldelucasciro@gmail.com>
2022-09-14 20:16:15 -0300

    app arreglada

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,8 @@
 // import { supabase } from "./supabase";
 import { Route, Switch } from "wouter";
 import { Form } from "./pages/form"
-<<<<<<< HEAD
 import { Login } from "./pages/login";
-import Home from "./pages/Home"
-=======
 import Home from "./pages/home"
->>>>>>> 46e4b26226b6bfab846c30cbe6cbd8a07b7201d0
 import Info from "./pages/info/info"
 import { Admin } from "./pages/admin"
 import { MercadoPago } from "./pages/mercadopago";
