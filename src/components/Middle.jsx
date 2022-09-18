c1674403867fce823f95b8ab5cca03e5137eb8c1
NicoTByte <nicolasgomez.110503@gmail.com>
2022-09-18 16:34:27 -0300

    Header

--- src/components/Middle.jsx
+++ src/components/Middle.jsx
@@ -1,30 +1,11 @@
 import React from "react";
 import { getImgUrl } from "../utils/getImgUrl";
+import Parallax from "./parallax";
 
 export function Middle() {
   return (
     <>
-
-      <div className="info">
-
-      </div>
-
-      <div className="containerR123">
-        <div className="r1">
-          <img className="imgMiddle" src={getImgUrl("a1.jpg")} />
-        </div>
-        <div className="r2">
-          <img className="imgMiddle" src={getImgUrl("a2.jpg")} />
-        </div>
-        <div className="r3">
-          <img className="imgMiddle" src={getImgUrl("a3.jpg")} />
-        </div>
-      </div>
-
-      {/* <div className="social">
-        <li><a href="#" className="facebook"><i className="fa-brands fa-facebook-f"></i></a></li>
-        <li><a href="#" className="instagram"><i className="fa-brands fa-instagram"></i></a></li>
-      </div> */}
+      <Parallax/>
       <iframe
         className="map"
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13121.066695941188!2d-58.3811!3d-34.6984532!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4a2c44eccfcb5756!2sBOMBEROS%20VOLUNTARIOS%20DE%20LAN%C3%9AS!5e0!3m2!1ses-419!2sar!4v1659825336950!5m2!1ses-419!2sar"
@@ -35,6 +16,7 @@ export function Middle() {
         referrerPolicy="no-referrer-when-downgrade"
       >
       </iframe>
+      <div className="elfsight-app-4de3859e-5d51-4a6b-aa31-d6d1b9d95bdd"></div>
     </>
   );
 }
---
+++ src/components/parallax/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Parallax() {
+    <div>
+        <div className='bging-1'>
+            <div className='caption'>
+                <span className='border'>
+                    Informacion
+                </span>
+            </div>
+        </div>
+    </div>
+}
