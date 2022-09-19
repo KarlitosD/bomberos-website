5a6c9f8d61f3e693911caff38b47e883838339bc
KarlitosD <carlosdcampos.04@gmail.com>
2022-09-18 21:04:33 -0300

    cache en javascript para las imagenes (creo que no es recomendado)

--- src/utils/getImgUrl.js
+++ src/utils/getImgUrl.js
@@ -1,3 +1,29 @@
-export function getImgUrl(file) {
-    return new URL(`/src/assets/img/${file}`, import.meta.url).href
+const cache = new Map()
+
+function blobToDataUrl(blob) {
+    return new Promise((res) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            res(reader.result);
+        }
+        reader.readAsDataURL(blob) 
+    })
+}
+
+export function getImgUrl(fileName) {
+    if(cache.has(fileName)) {
+        console.log("Hola hermosa")
+        return cache.get(fileName)
+    }
+    const imageUrl = new URL(`/src/assets/img/${fileName}`, import.meta.url).href
+    // const imageBlob = await fetch(imageUrl).then(res => res.blob())
+    // const dataUrl = await blobToDataUrl(imageBlob)
+    // cache.set(fileName, dataUrl)
+    // return null
+    cache.set(fileName, "data:")
+    fetch(imageUrl)
+        .then(res => res.blob())
+        .then(blobToDataUrl)
+        .then(dataUrl => cache.set(fileName, dataUrl))
+    return "data:"
 }
