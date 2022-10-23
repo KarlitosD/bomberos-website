814dd5dc91672452c5a30b9563af09edaf46b8e4
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-23 19:18:54 -0300

    hacer que el login funciona minimamente

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,22 +1,47 @@
 import "./style.css";
 import { Header } from "@/components/Header";
-import { useState, useEffect } from "react";
-import { getAssociates, createAssociate } from "@/services/associates";
-
+import { useEffect, useState } from "react";
+import { supabase } from "@/supabase.js"
+// import { createAssociate, getAssociates } from "@/services/associates";
 
 export function Login() {
+  const [loading, setLoading] = useState(false)
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  })
 
-  return (<>
-  <form>
-    <h1>Iniciar Sesion</h1>
-    <label>Usuario</label>
-    <input type="text"></input>
-    <label>Constraseña</label>
-    <input type="password"></input>
-    <button>Entrar</button>
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      setLoading(true)
+      const { error } = await supabase.auth.signIn(credentials)
+      if(error) throw error
+    } catch (error) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
+    }
+  }
 
-  </form>
+  const createHandleChange = name => event => {
+    setCredentials(credentials => ({ ...credentials, [name]: event.target.value }))
+  } 
   
-  </>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h1>Iniciar Sesion</h1>
+        <label>
+          Usuario
+          <input type="email" onChange={createHandleChange("email")} name="email" required />
+        </label>
+        <label>
+          Constraseña
+          <input type="password" onChange={createHandleChange("password")} name="password" minLength={6} required />
+        </label>
+        <button disabled={loading}>Entrar</button>
+      </form>
+    </>
   );
 }
