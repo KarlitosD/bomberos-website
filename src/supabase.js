28a51b4d9757f8db6a90247769b7a2204e1bfffe
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-23 19:16:50 -0300

    mostrar en consola cuando  la session cambia

--- src/supabase.js
+++ src/supabase.js
@@ -4,3 +4,8 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
+//TODO: Eliminar despues
+supabase.auth.onAuthStateChange(() => {
+    console.log("Estado del auth cambio")
+})
