036a5a46206f65ff8b50fef6cc30a72494dfdf5d
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-24 16:09:14 -0300

    cambios menores en la pagina de preguntas y respuestas

--- src/pages/faqPage/index.jsx
+++ src/pages/faqPage/index.jsx
@@ -1,66 +1,58 @@
 import { Header } from "@/components/Header";
-import {useState} from "react"
-import "./style.css"
+import { useState } from "react";
+import "./style.css";
 
-export function Faq(){
-	
+const Question = (props) => {
+    const [isClicked, setIsClicked] = useState(false);
+    return (
+      <div className="question">
+        <div className="barra1" onClick={() => setIsClicked(!isClicked)}>
+          <h3>{props.question}</h3>
+          {isClicked ? <h2>-</h2> : <h2>+</h2>}
+        </div>
+        <div className="barra2">
+          {isClicked ? <p>{props.answer}</p> : <p></p>}
+        </div>
+      </div>
+    );
+  }
 
-	const Pregunta = (props) =>{
-		const [isClicked, setIsClicked] = useState(false)
-		return(
-			<div className="pregunta">
-				<div className="barra1" onClick={() => setIsClicked(!isClicked)}>
-					<h3>{props.pregunta}</h3>		
-					{isClicked ? <h2>-</h2> : <h2>+</h2>}		
-				</div>
-				<div className="barra2">
-					{
-						isClicked
-						?
-						<p>{props.respuesta}</p>
-						:
-						<p></p>
-					}
-				</div>
-			</div>
-		)
-	}
-  return(
+export function Faq() {
+  return (
     <>
-		<Header />
-		<div>
-			<div className="acordion">
-				<Pregunta
-				pregunta={"¿Que diferencia hay entre ser Socio, y  solamente Donar?"}
-				respuesta={"No se amigo, me mataste"}
-				/>
-				<Pregunta
-				pregunta={"Al inscribirme como bombero ¿Tengo un salario o pago?"}
-				respuesta={"No"}
-				/>
-				<Pregunta
-				pregunta={"¿Con que beneficios cuenta un bombero?"}
-				respuesta={"Algo de la jubilacion y honor"}
-				/>
-				<Pregunta
-				pregunta={"¿Como bombero, puedo contar con otro oficio?"}
-				respuesta={"Si"}
-				/>
-				<Pregunta
-				pregunta={"¿Que diferencia hay entre un bombero voluntario, y de la ciudad?"}
-				respuesta={"uno es voluntario y otro de la ciudad"}
-				/>
-				<Pregunta
-				pregunta={"¿Los bomberos se manejan con horarios de trabajo?"}
-				respuesta={"no"}
-				/>
-				<Pregunta
-				pregunta={"¿En donde se encuentran repartido los destacamentos?"}
-				respuesta={"por ahi"}
-				/>
-			</div>
-				
-		</div>
+      <Header />
+      <div>
+        <div className="acordion">
+          <Question
+            question={"¿Que diferencia hay entre ser Socio, y  solamente Donar?"}
+            answer={"No se amigo, me mataste"}
+          />
+          <Question
+            question={"Al inscribirme como bombero ¿Tengo un salario o pago?"}
+            answer={"No"}
+          />
+          <Question
+            question={"¿Con que beneficios cuenta un bombero?"}
+            answer={"Algo de la jubilacion y honor"}
+          />
+          <Question
+            question={"¿Como bombero, puedo contar con otro oficio?"}
+            answer={"Si"}
+          />
+          <Question
+            question={"¿Que diferencia hay entre un bombero voluntario, y de la ciudad?"}
+            answer={"uno es voluntario y otro de la ciudad"}
+          />
+          <Question
+            question={"¿Los bomberos se manejan con horarios de trabajo?"}
+            answer={"no"}
+          />
+          <Question
+            question={"¿En donde se encuentran repartido los destacamentos?"}
+            answer={"por ahi"}
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 }
