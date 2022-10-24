7d58f1c76f05fc54adf1345ac1ed8ac1b3f403b5
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-24 12:45:23 -0300

    tabulaciones en app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import { Route, Switch } from "wouter";
 import { Form } from "./pages/form";
 import { Login } from "./pages/login";
-import { Faq } from "./pages/faqPage"
+import { Faq } from "./pages/faqPage";
 import Home from "./pages/home";
 import Info from "./pages/info/info";
 import { NestedRouter } from "@/components/NestedRouter";
@@ -14,40 +14,39 @@ import { Admin } from "./pages/admin";
 import { Profile } from "./pages/profile";
 import { FormApp } from "./pages/formApp";
 
-
 function App() {
   return (
     <>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-          <Route path="/form">
-            <Form/>
-          </Route>
-		  <Route path="/faq">
-			<Faq/>
-		  </Route>
-          <Route path="/formApp">
-            <FormApp/>
-          </Route>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/info">
-            <Info />
-          </Route>
-          <Route path="/admin">
-            <Admin/>
-          </Route>
-        
-          <Route path="/profile">
-            <Profile/>
-          </Route>
-          <Route>
-            Pagina 404
-          </Route>
-        </Switch>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/form">
+          <Form />
+        </Route>
+        <Route path="/faq">
+          <Faq />
+        </Route>
+        <Route path="/formApp">
+          <FormApp />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/info">
+          <Info />
+        </Route>
+        <Route path="/admin">
+          <Admin />
+        </Route>
+
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route>
+          Pagina 404
+        </Route>
+      </Switch>
     </>
   );
 }
