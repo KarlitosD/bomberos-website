8f2ef0fd16bc56663688a8e9f1b6f20dd16df976
cajitadecarton <joaconizzo@gmail.com>
2022-10-27 00:25:41 -0300

    borrados comentarios de admin

--- src/pages/admin/associates.jsx
+++ src/pages/admin/associates.jsx
@@ -78,65 +78,6 @@ export const Associates = () => {
           }}
         />
       </div>
-      {/* <table className="table">
-        <thead>
-          <tr>
-            <th onClick={createHandleClick("dni")} className="theadItem">
-              DNI
-            </th>
-            <th onClick={createHandleClick("name")} className="theadItem">
-              NOMBRE
-            </th>
-            <th onClick={createHandleClick("surname")} className="theadItem">
-              APELLIDO
-            </th>
-            <th onClick={createHandleClick("email")} className="theadItem">
-              CORREO ELECTRÓNICO
-            </th>
-            <th onClick={createHandleClick("address")} className="theadItem">
-              DIRECCIÓN
-            </th>
-            <th onClick={createHandleClick("phone_num")} className="theadItem">
-              NÚMERO DE TELÉFONO
-            </th>
-            <th
-              onClick={createHandleClick("associate_num")}
-              className="theadItem"
-            >
-              NÚMERO DE SOCIO
-            </th>
-            <th onClick={createHandleClick("approved")} className="theadItem">
-              APROBADO
-            </th>
-            <th className="theadItem">ELIMINAR SOCIO</th>
-          </tr>
-        </thead>
-        <tbody>
-          {visibleAssociates?.map((associate) => (
-            <tr className="dataRow" key={associate.dni}>
-              <td className="tdata">{associate.dni}</td>
-              <td className="tdata">{associate.name}</td>
-              <td className="tdata">{associate.surname}</td>
-              <td className="tdata">{associate.email}</td>
-              <td className="tdata">{associate.address}</td>
-              <td className="tdata">{associate.phone_num}</td>
-              <td className="tdata">{associate.associate_num}</td>
-              <td className="tdata">
-                <input type="checkbox" checked={associate.approved} readOnly  />
-              </td>
-              <td className="tdata">
-                <button
-                  className="delBtn"
-                  onClick={() =>
-                    handleDel(associate.dni)}
-                >
-                  Dar de baja
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table> */}
     </>
   );
 };
