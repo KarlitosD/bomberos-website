5c83fd09850a706b7ad19921f0da4e878084a513
KarlitosD <carlosdcampos.04@gmail.com>
2022-10-26 23:53:19 -0300

    boton para cerrar session

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 import { Redirect } from "wouter";
+import { supabase } from "@/supabase"
 import { useAuth } from "@/hooks/useAuth.js"
 import { getAssociate } from "@/services/associates";
 import "./style.css";
 
 export function Profile() {
-  const { user } = useAuth()
+  const { user = true } = useAuth()
   const [associate, setAssociate] = useState({})
 
   useEffect(() => {
     if(user)
       getAssociate(user.id).then(associate => setAssociate(associate))
   }, [])
+
+  if(!user)
+    return <Redirect to="/admin" />
 
   if(associate?.role === "admin")
     return <Redirect to="/admin" />
@@ -20,14 +24,13 @@ export function Profile() {
     <>
       <div>
         <h1>Hola</h1>
-        <button>Cerrar session</button>
+        <button onClick={() => supabase.auth.signOut()}>Cerrar session</button>
       </div>
 
       <div>
         Carnet
         <button>Descargar carnet</button>
       </div>
-
     </>
   );
 }
