c1df9f931654207d8a68f746298a71648b9fa198
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-14 19:41:04 -0300

    mover columnas a una variable fuera del componente

--- src/pages/admin/associates.jsx
+++ src/pages/admin/associates.jsx
@@ -1,78 +1,39 @@
 import { Grid } from "gridjs-react";
 // import { useAtom } from "jotai";
 // import { associatesAtom } from "@/atoms/associates";
-import { useLoaderData, json } from "react-router-dom";
-import {
-  deleteAssociate,
-  getAssociates,
-  updateAssociate,
-} from "@/services/associates";
+import { json, useLoaderData } from "react-router-dom";
+import { getAssociates, updateAssociate } from "@/services/associates";
 import styles from "./style.module.css";
 
-export const loader = async () => {
-  const associates = await getAssociates()
-  return json(associates)
-}
+const COLUMNS = [
+  { id: "dni", name: "DNI" },
+  { id: "name", name: "Nombre" },
+  { id: "surname", name: "Apellido" },
+  { id: "email", name: "Email" },
+  { id: "address", name: "Dirección" },
+  { id: "phoneNumber", name: "Número de Teléfono" },
+  { id: "cellphoneNumber", name: "Número de Celular" },
+  { id: "plan", name: "Plan" },
+  { id: "associateNumber", name: "Número de Socio" },
+  { id: "paymentMethod", name: "Método de Pago" },
+  { id: "approved", name: "Aprobado" },
+  { id: "role", name: "Rol" },
+];
 
+export const loader = async () => {
+  const associates = await getAssociates();
+  return json(associates);
+};
 
 export const Associates = () => {
-  const associates = useLoaderData() 
+  const associates = useLoaderData();
   return (
     <>
       <div className={styles.gridContainer}>
         <Grid
           data={associates}
           fixedHeader={true}
-          columns={[
-            {
-              id: "dni",
-              name: "DNI",
-            },
-            {
-              id: "name",
-              name: "Nombre",
-            },
-            {
-              id: "surname",
-              name: "Apellido",
-            },
-            {
-              id: "email",
-              name: "Email",
-            },
-            {
-              id: "address",
-              name: "Dirección",
-            },
-            {
-              id: "phoneNumber",
-              name: "Número de Teléfono",
-            },
-            {
-              id: "cellphoneNumber",
-              name: "Número de Celular",
-            },
-            {
-              id: "plan",
-              name: "Plan",
-            },
-            {
-              id: "associateNumber",
-              name: "Número de Socio",
-            },
-            {
-              id: "paymentMethod",
-              name: "Método de Pago",
-            },
-            {
-              id: "approved",
-              name: "Aprobado",
-            },
-            {
-              id: "role",
-              name: "Rol",
-            },
-          ]}
+          columns={COLUMNS}
           search={true}
           pagination={{
             enabled: true,
