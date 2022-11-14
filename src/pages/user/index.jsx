aa88a34e4309f0723174c8ccb667f225deff7148
cajitadecarton <joaconizzo@gmail.com>
2022-11-13 22:30:32 -0300

    carnet descargable

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,25 +1,46 @@
 import React from "react";
 import style from "./style.module.css";
-import getAssociates from "@/services/associates";
-import { toPng } from "html-to-image";
+import { useEffect, useState, useCallback, useRef } from "react";
+import { getAssociate } from "@/services/associates";
+import { useAtom } from "jotai";
+import { associatesAtom } from "../../atoms/associates";
+import { useAuth } from "../../hooks/useAuth";
+import { toJpeg } from "html-to-image";
 
 export function User() {
-  const [associates, setAssociates] = useAtom(associatesAtom);
+  const { user } = useAuth();
+  const [associate, setAssociate] = useState(null);
+  const ref = useRef(null);
 
   useEffect(() => {
-    if (associates.length === 0) {
-      fetchAssociates();
-    }
+    fetchAssociate();
   }, []);
 
-  async function fetchAssociates() {
-    const data = await getAssociates();
-    setAssociates(data);
+  const onButtonClick = () => {
+    if (ref.current === null) {
+      return;
+    }
+    toJpeg(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "carnet-socio.jpeg";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log("uwu, tuvimos um pequeuño ewowsito 👉👈" + err);
+      });
+  };
+
+  async function fetchAssociate() {
+    const data = await getAssociate(user?.id);
+    setAssociate(data);
   }
+
   return (
     <>
       <div className={style.container}>
-        <div className={style.credential}>
+        <div ref={ref} className={style.credential}>
           <div className={style.borderT}>
             <h1>SOCIO</h1>
             <h1>
@@ -28,14 +49,14 @@ export function User() {
           </div>
           <div className={style.center}>
             <div className={style.photo} />
-            <div className={style.borderR}>
+            <div className={style.rightSide}>
               <p className={style.certi}>
                 Certifico que el titular de la presente Señor/a
               </p>
-              <p>Nombre:</p>
-              <p>Apellido:</p>
-              <p>DNI:</p>
-              <p>Numero de socio:</p>
+              <p>Nombre: {associate?.name}</p>
+              <p>Apellido: {associate?.surname}</p>
+              <p>DNI: {associate?.dni}</p>
+              <p>Numero de socio: {associate?.associateNumber}</p>
               <p className={style.isAssociate}>es SOCIO</p>
             </div>
           </div>
@@ -43,6 +64,7 @@ export function User() {
           <div className={style.borderB} />
         </div>
       </div>
+      <button onClick={onButtonClick} className={style.downloadBtn}>DESCARGAR</button>
     </>
   );
 }
