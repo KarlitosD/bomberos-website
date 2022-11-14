88686448248693370956ddc0fa1b33d0a562b525
cajitadecarton <joaconizzo@gmail.com>
2022-11-13 21:15:01 -0300

    htmlToImage instalado y carnet conectado a base de datos

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,32 +1,46 @@
 import React from "react";
 import style from "./style.module.css";
+import getAssociates from "@/services/associates";
+import { toPng } from "html-to-image";
 
 export function User() {
+  const [associates, setAssociates] = useAtom(associatesAtom);
+
+  useEffect(() => {
+    if (associates.length === 0) {
+      fetchAssociates();
+    }
+  }, []);
+
+  async function fetchAssociates() {
+    const data = await getAssociates();
+    setAssociates(data);
+  }
   return (
     <>
-      <div className={style.contenedorNicoPuto}>
-        <div className={style.credencialNicoPuto}>
-          <div className={style.borderTopNicoPuto}>
+      <div className={style.container}>
+        <div className={style.credential}>
+          <div className={style.borderT}>
             <h1>SOCIO</h1>
             <h1>
               del cuerpo activo de la sociedad de Bomberos Voluntarios de Lanús
             </h1>
           </div>
-          <div className={style.centroNicoPuto}>
-            <div className={style.fotoNicoPuto} />
-            <div className={style.borderRightNicoPuto}>
-              <p className={style.certNicoPuto}>
+          <div className={style.center}>
+            <div className={style.photo} />
+            <div className={style.borderR}>
+              <p className={style.certi}>
                 Certifico que el titular de la presente Señor/a
               </p>
               <p>Nombre:</p>
               <p>Apellido:</p>
               <p>DNI:</p>
               <p>Numero de socio:</p>
-              <p className={style.cert1NicoPuto}>es SOCIO</p>
+              <p className={style.isAssociate}>es SOCIO</p>
             </div>
           </div>
-          <p className={style.firmNicoPuto}>Jefe_de_cuerpo Presidente</p>
-          <div className={style.borderBottomNicoPuto} />
+          <p className={style.signature}>Jefe_de_cuerpo Presidente</p>
+          <div className={style.borderB} />
         </div>
       </div>
     </>
