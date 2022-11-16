924f29deec0f8777b1e9c40de2f83898590496f9
cajitadecarton <joaconizzo@gmail.com>
2022-11-16 19:42:06 -0300

    tutorial con imagenes placeholder

--- src/pages/infoAss/index.jsx
+++ src/pages/infoAss/index.jsx
@@ -1,21 +1,22 @@
 import { Link } from "react-router-dom";
 import styles from "./style.module.css";
 import { useState } from "react";
+import { Tutorial } from "../../components/Tutorial";
 
-  const Question = (props) => {
-    const [isClicked, setIsClicked] = useState(false);
-    return (
-      <div className={styles.question}>
-        <div className={styles.barra1} onClick={() => setIsClicked(!isClicked)}>
-          <h3>{props.question}</h3>
-          {isClicked ? <h2>-</h2> : <h2>+</h2>}
-        </div>
-        <div className={styles.barra2}>
-          {isClicked ? <p className={styles.answer}>{props.answer}</p> : <p></p>}
-        </div>
+const Question = (props) => {
+  const [isClicked, setIsClicked] = useState(false);
+  return (
+    <div className={styles.question}>
+      <div className={styles.barra1} onClick={() => setIsClicked(!isClicked)}>
+        <h3>{props.question}</h3>
+        {isClicked ? <h2>-</h2> : <h2>+</h2>}
       </div>
-    );
-  };
+      <div className={styles.barra2}>
+        {isClicked ? <p className={styles.answer}>{props.answer}</p> : <p></p>}
+      </div>
+    </div>
+  );
+};
 
 export function InfoAss() {
   const Card = ({ title, description }) => {
@@ -34,9 +35,7 @@ export function InfoAss() {
       <div className={styles.container}>
         <div className={styles.acordion}>
           <Question
-            question={
-              "¿Que son los socios?"
-            }
+            question={"¿Que son los socios?"}
             answer={
               "Los socios para nuestro son miembros y personas como tu, que pueden pagar una suscripcion mensual o anual con la que pueden apoyar a el cuartel de bomberos de lanus."
             }
@@ -44,19 +43,22 @@ export function InfoAss() {
           <Question
             question={"¿Que beneficios tienen?"}
             answer={
-              'Actualmente, los socios que estan inscriptos a nuestro destacamento, pueden obtener algunos beneficios como *completar*'
+              "Actualmente, los socios que estan inscriptos a nuestro destacamento, pueden obtener algunos beneficios como *completar*"
             }
           />
           <Question
             question={"Me interesa ser socio. ¿Pero como me inscribo?"}
             answer={
-              <p>
-              El formulario es este:{" "}
-              <Link to="/formulario/socios">
-                {" "}
-                <a>¡Da click Aqui!</a>{" "}
-              </Link>
-            </p>
+              <div>
+                <Tutorial />
+                <p>
+                  El formulario es este:{" "}
+                  <Link to="/formulario/socios">
+                    {" "}
+                    <a>¡Da click Aqui!</a>{" "}
+                  </Link>
+                </p>
+              </div>
             }
           />
         </div>
