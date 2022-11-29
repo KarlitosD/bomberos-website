2ddbe3d376343d52562f4a9dcda77cb530242595
cajitadecarton <joaconizzo@gmail.com>
2022-11-29 00:27:05 -0300

    imagen insertada por usuario casi terminado

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { json, redirect, useLoaderData } from "react-router-dom";
 import { toJpeg } from "html-to-image";
 import { getAssociate } from "@/services/associates";
@@ -18,6 +18,8 @@ export const loader = async () => {
 export function User() {
   const associate = useLoaderData();
   const ref = useRef(null);
+  const [image, setImage] = useState(null);
+  const [imageUrl, setImageUrl] = useState("");
 
   const onBtnDNLDClick = () => {
     if (ref.current === null) {
@@ -34,65 +36,100 @@ export function User() {
         console.log("uwu, tuwimos um peweuño ewowsito 👉👈" + err);
       });
   };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let imageUrl = "";
 
-  let toggleForm = false;
-  const onBtnEditClick = () => {
-    toggleForm = !toggleForm;
-    console.log(toggleForm);
+    if (image) {
+      const { data, error } = await supabase.storage
+        .from("associate-image")
+        .upload(`${Date.now()}_${image.name}`, image);
+
+      if (error) {
+        console.log(error);
+      }
+      if (data) {
+        setImageUrl(data.Key);
+        imageUrl = data.Key;
+      }
+    }
+
+    const [data, error] = await supabase.from("associates").upsert({
+      imageUrl: imageUrl,
+    });
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      console.log("Imagen actualizada");
+    }
   };
+
   return (
     <>
       <div className={style.container}>
-        <div>
-          <div ref={ref} className={style.credential}>
-            <div className={style.borderT}>
-              <h1 className={style.title}>
-                SOCIO
-                <br />
-                del cuerpo activo de la sociedad de Bomberos Voluntarios de
-                Lanús
-              </h1>
-            </div>
-            <div>
-              <div className={style.center}>
-                <div className={style.photo} />
-                <div className={style.rightSide}>
-                  <p className={style.certi}>
-                    Certifico que el titular de la presente Señor/a
-                  </p>
-                  <div className={style.dataContainer}>
-                    <p className={style.data}>
-                      <b>Nombre/s:</b> {associate?.name}
-                    </p>
-                    <p>
-                      <b>Apellido/s:</b> {associate?.surname}
-                    </p>
-                    <p>
-                      <b>DNI:</b> {associate?.dni}
-                    </p>
-                    <p>
-                      <b>Número de socio:</b> {associate?.associateNumber}
-                    </p>
+        <div className={style.credentialContainer}>
+          <div>
+            <div ref={ref} className={style.credential}>
+              <div className={style.borderT}>
+                <h1 className={style.title}>
+                  SOCIO
+                  <br />
+                  del cuerpo activo de la sociedad de Bomberos Voluntarios de
+                  Lanús
+                </h1>
+              </div>
+              <div>
+                <div className={style.center}>
+                  <div className={style.photo}>
+                    {/* {imageUrl ? <img src={ [LINK] imageUrl}/> : <h2>Sin Imagen</h2>} */}
                   </div>
-                  <div className={style.isAssociateContainer}>
-                    <p className={style.isAssociate}>es SOCIO</p>
+                  <div className={style.rightSide}>
+                    <p className={style.certi}>
+                      Certifico que el titular de la presente Señor/a
+                    </p>
+                    <div className={style.dataContainer}>
+                      <p className={style.data}>
+                        <b>Nombre/s:</b> {associate?.name}
+                      </p>
+                      <p>
+                        <b>Apellido/s:</b> {associate?.surname}
+                      </p>
+                      <p>
+                        <b>DNI:</b> {associate?.dni}
+                      </p>
+                      <p>
+                        <b>Número de socio:</b> {associate?.associateNumber}
+                      </p>
+                    </div>
+                    <div className={style.isAssociateContainer}>
+                      <p className={style.isAssociate}>es SOCIO</p>
+                    </div>
                   </div>
                 </div>
+                <div className={style.signaturesContainer}>
+                  <p className={style.signature}>Jefe de Cuerpo</p>
+                  <p className={style.signature}>Presidente</p>
+                </div>
               </div>
-              <div className={style.signaturesContainer}>
-                <p className={style.signature}>Jefe de Cuerpo</p>
-                <p className={style.signature}>Presidente</p>
-              </div>
+              <div className={style.borderB} />
             </div>
-            <div className={style.borderB} />
+            <button onClick={onBtnDNLDClick} className={style.btnDNLD}>
+              DESCARGAR
+            </button>
           </div>
-          <button onClick={onBtnDNLDClick} className={style.btnDNLD}>
-            DESCARGAR
-          </button>
-          <button onClick={onBtnEditClick} className={style.btnEdit}>
-            Editar
-          </button>
         </div>
+        <form onSubmit={handleSubmit} className={style.updatePicture}>
+          <input
+            type="file"
+            accept="image/jpeg image/png"
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+          <button type={"submit"} className={style.btnEdit}>
+            Actualizar Foto
+          </button>
+        </form>
       </div>
     </>
   );
