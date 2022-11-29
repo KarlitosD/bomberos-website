23ee011e545ccdacea20875b236dd54011e16b59
KarlitosD <carlosdcampos.04@gmail.com>
2022-11-29 14:56:20 -0300

    limpiar dependencias

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,5 @@ export default defineConfig({
        "@": path.resolve(__dirname, "./src")
     }
   },
-  plugins: [react(), ViteFaviconsPlugin("src/assets/img/logo.png") ]
+  plugins: [react()]
 })
